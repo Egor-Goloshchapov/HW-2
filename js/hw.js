80b18ceec8b19ca39e5b5319213905c0980e1762
Egor-Goloshchapov <141495264+Egor-Goloshchapov@users.noreply.github.com>
2024-07-28 00:18:34 +0300

    did hw-2

---
+++ js/hw.js
@@ -0,0 +1,51 @@
+const a = 0 + 32;
+console.log(a);
+
+const formula = "Formula = Celsius + Fahrenheit";
+console.log(formula);
+
+const b = 24 * 31;
+const c = 60 * 24 * 31;
+console.log(b , c)
+const time = "Hours and minutes in July";
+console.log(time);
+
+
+
+let health = 100;
+let energy = 100;
+
+console.log(`Initial health level: ${health}`);
+console.log(`Initial energy level: ${energy}`);
+
+health -= 20;
+energy -= 15;
+
+console.log(`Health level after attack: ${health}`);
+console.log(`Energy level after attack: ${energy}`);
+
+let price = 100;
+let sale = 10;
+let priceEnd = price - sale;
+console.log(priceEnd);
+
+console.log(Math)
+console.log(Math.floor(3.453))
+
+let number = "45.67";
+let num = parseFloat(number);
+console.log(`Number: ${num}`);
+
+let number1 = "123";
+let num1 = parseInt(number1, 10);
+console.log(`Number: ${num1}`);
+
+let number2 = 144;
+let num2 = Math.sqrt(number2);
+console.log(`The square root of the number ${number2} is equal to: ${num2}`);
+
+
+
+
+
+
